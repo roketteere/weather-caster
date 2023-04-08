baea965fc7e3f2afdb585e468177c97da7404cd1
Joel Perez <jxp489@gmail.com>
2023-04-08 09:33:40 -0700

    began implementing query to open weather map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
-var base = 'https://www.mapquestapi.com/search/v3/prediction?key=ceiWumpWrG5aqAOi4bsRb8BIkjPl3vtP&limit=5&collection=address,city&q=';
-
-
 var cities = [];
 
 
 var searchButton = document.querySelector("#search-button");
-var searchBox = document.querySelector("#search")
-// var resultContainer = document.querySelector("#results");
+var searchBox = document.querySelector("#search");
+var resultContainer = document.querySelector("#results");
+
+var resultBlock = document.querySelector("#result-block");
+
+// TODO Create dropdown list to select city so that we can take the city and pass it to the Geocoding API
 
 // Fetch function to get our data, slice what we need for each item in the results found
 function fetchMapData(keyword) {
 
-
     fetch(`https://www.mapquestapi.com/search/v3/prediction?key=ceiWumpWrG5aqAOi4bsRb8BIkjPl3vtP&limit=5&collection=address,city&q=${keyword}`).then(promise => promise.json()).then(data => { // JSON.stringify(data);
-        console.log(data.results);
-        for (var i = 0; i < data.results.length; i++) {
+        console.log(data.results)
 
+        for (var i = 0; i < data.results.length; i++) {
+            var listCity = data.results[i].name;
             var city = data.results[i].name;
 
             var state = data.results[i].place.properties.state;
@@ -26,10 +27,16 @@ function fetchMapData(keyword) {
 
             var latitude = data.results[i].place.geometry.coordinates[1];
 
+            var resultList = document.createElement("option");
+            resultList.text = listCity;
+            resultList.value = listCity;
+            resultBlock.appendChild(resultList);
+            resultContainer.setAttribute("style", "display:block;");
+            getWeatherAPI(longitude, latitude);
 
-            cities.push([city, state, latitude, latitude]);
+            cities.push([city, state, longitude, latitude]);
 
-            console.log('Last Log: \n' + cities[i]);
+            // console.log('Last Log: \n' + cities[i]);
             console.log(`City: ${city}\nState: ${state}\nLongitude: ${longitude}\nLatitude: ${latitude}`)
 
 
@@ -38,6 +45,33 @@ function fetchMapData(keyword) {
 
     });
 }
+
+function getWeatherAPI(city) {
+    var API_KEY = '501097da5c0ccc04bda86f2d077d16bb';
+
+    var API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}limit=5&appid=${API_KEY}`
+
+
+    fetch(API_URL).then(api => api.json()).then(api_data => {
+        console.log(`City Name:${
+            api_data[0].name
+        }\nLongitude:${
+            api_data[0].lon
+        }\nLatitude:${
+            api_data[0].lat
+        }`);
+    });
+
+
+}
+
+resultContainer.addEventListener('click', function (event) {
+    var city = event.target.value
+    // console.log('CITY CLICKED: ', city);
+    getWeatherAPI(city)
+
+
+})
 // Event listener for the search button. If the user hasn't typed anything it, it will display an alert letting them know they need to enter search parameters first. This way they can't use up API requests unnecessarily. We pass in
 searchButton.addEventListener('click', function (event) {
     var info = searchBox.value;
